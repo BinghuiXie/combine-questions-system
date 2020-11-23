31b696ca46de802196ba0b5adebb8c02f4540d0c
不会拉小提琴的左脚 <tikitaka@buhuilaxiaotiqindezuojiaodeMacBook-Pro.local>
2020-11-23 10:52:48 +0800

    fix: change vue version and set up new vue-tsx template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+    render() {
+        return (
+            <div id='app'>
+                hhhhhhhh
+            </div>
+        )
+    }
+}
